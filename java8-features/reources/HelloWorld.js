d2be0fba2bdc7b1b4edde6393efc0a6bca37d12d
Mohsin Khursheed <momo@Mohsins-MacBook-Air.local>
2017-09-12 20:19:41 +0530

    Java8 Hello World | Nashorn js

---
+++ java8-features/reources/HelloWorld.js
@@ -0,0 +1,8 @@
+function printHelloWorld(){
+	print("Hello World From a JS file!");
+}
+
+function helloUser(name){
+	var name;
+	print("Hello "+name +"!")
+}
---
+++ java8-features/src/com/sample/nashorn/HelloWorld.js
@@ -0,0 +1,8 @@
+function hello() {
+	print("Hello World From JavaScript file using Java Nashorn!");
+}
+
+function helloUser(name) {
+	var name;
+	print("Hello " + name + "!")
+}
